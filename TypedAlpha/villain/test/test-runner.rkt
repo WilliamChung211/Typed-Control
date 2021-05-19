98f0c5ab4224c668db6789069ce99d518b3139d9
WilliamChung211 <Wchung12326@gmail.com>
2021-05-19 11:26:21 -0400

    Update test-runner.rkt

--- TypedAlpha/villain/test/test-runner.rkt
+++ TypedAlpha/villain/test/test-runner.rkt
@@ -2,6 +2,8 @@
 (provide test-runner)
 (require rackunit)
 
+;; Uncomment the commented tests to check for errors
+
 (define (test-runner run)
   ;; Alpha examples
   (check-equal? (run 7) 7)
@@ -9,7 +11,7 @@
   (check-equal? (run '(add1 (add1 7))) 9)
    (check-equal? (run '(if #f (sub1 7) 4)) 4)
   ;;(check-equal? (run '(add1 (zero? 7))) 'err)
- ; (check-equal? (run '(if (sub1 7) 5 4)) 'err)
+ ;;(check-equal? (run '(if (sub1 7) 5 4)) 'err)
   ;;(check-equal? (run '(if 7 #f #t)) 'err)
   ;;(check-equal? (run '(if #t 5 #t)) 'err)
 
